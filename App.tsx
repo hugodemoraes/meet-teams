17aae9d483cdf9dde85c2fa6cfcbe04e618fe9aa
Hugo de Moraes <hugoc.bmoraes@gmail.com>
2023-06-08 16:14:24 -0300

    :lipstick: add Roboto fonts

--- App.tsx
+++ App.tsx
@@ -1,12 +1,20 @@
 import { ThemeProvider } from "styled-components";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 
 import Groups from "@screens/Groups";
+import { Loading } from "@components/Loading";
 import theme from "./src/theme";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {!fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
