752c778e8cdd577aae22e1435162125bf74a0c1d
Hugo de Moraes <hugoc.bmoraes@gmail.com>
2023-06-08 22:31:41 -0300

    :sparkles: create groups screen

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,18 +1,33 @@
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+
+import { Header } from "@components/Header";
+import { Highlight } from "@components/Highlight";
+import { GroupCard } from "@components/GroupCard";
+import { EmptyList } from "@components/EmptyList";
+
+import { Container } from "./styles";
+import { FlatList } from "react-native";
+import { Button } from "@components/Button";
 
 export default function Groups() {
+  const [groups, setGroups] = useState<string[]>([]);
+
   return (
-    <View style={styles.container}>
-      <Text>Groups</Text>
-    </View>
+    <Container>
+      <Header />
+      <Highlight title="Turmas" subtitle="jogue com a sua turma" />
+
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        contentContainerStyle={!groups.length && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <EmptyList message="Que tal cadastrar a primeira turma?" />
+        )}
+        renderItem={({ item }) => <GroupCard title={item} />}
+      />
+
+      <Button description="Criar nova turma" />
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
